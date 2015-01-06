134c01af6bfabd8b92d2eb8ae1d55a3ee3214801
Chtyllhu <d.a.shimanov@gmail.com>
2015-01-06 20:23:14 +0600

    MVVM
    Первое простейшее использование MVVM

---
+++ MvvmSimple/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+using GalaSoft.MvvmLight;
+
+namespace MvvmSimple.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+
+        public string Message
+        {
+            get
+            {
+                return "Hello MVVM!";
+            }
+        }
+        
+        public MainViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+        }
+    }
+}
